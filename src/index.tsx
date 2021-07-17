740ea710a02f66949a701687d1636991eccd2496
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-07-18 02:02:21 +0300

    Add styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import "./theme/global.less";
 
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
