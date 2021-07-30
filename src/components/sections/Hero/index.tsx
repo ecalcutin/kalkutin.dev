72bbf894d359cc3187feaef05ff146706571a325
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-07-30 19:46:35 +0300

    Add cetification links

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 
 const Hero = () => {
   const [isMounted, setIsMounted] = useState(null);
-  
+
   useLayoutEffect(() => {
     setIsMounted(true);
   });
@@ -19,8 +19,24 @@ const Hero = () => {
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <p>
-      I'm a certified Node.js (JSNAD/JSNSD) &amp; Javascript engineer
-      specializing in building exceptional digital experiences.
+      I'm a certified Node.js{" "}
+      <a
+        target="_blank"
+        title="JSNAD: OpenJS Node.js Application Developer"
+        href="https://www.credly.com/badges/486a76a8-4bc3-40d7-bd63-30f77c68fbca/linked_in_profile"
+      >
+        JSNAD
+      </a>{" "}
+      &amp;{" "}
+      <a
+        target="_blank"
+        title="JSNSD: OpenJS Node.js Services Developer"
+        href="https://www.credly.com/badges/b92c0c45-0604-4321-b696-607bb508d1c8?source=linked_in_profile"
+      >
+        JSNSD
+      </a>{" "}
+      Javascript engineer specializing in building exceptional digital
+      experiences.
     </p>
   );
   const five = (
