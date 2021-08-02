ac5051900bcbdbe9c5da2366caee09cd7fc18617
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-08-03 02:07:30 +0300

    Move links

--- src/components/Nav/links.ts
+++ src/components/Nav/links.ts
@@ -8,13 +8,13 @@ const navLinks = [
     url: "/#experience",
   },
   {
+    name: "Contact",
+    url: "/#contact",
+  },
+  {
     name: "Blog",
     url: "https://blog.kalkutin.dev",
     target: "_blank",
-  },
-  {
-    name: "Contact",
-    url: "/#contact",
   },
 ];
 
