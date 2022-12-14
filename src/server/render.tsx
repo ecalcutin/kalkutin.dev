16ca0304e10e2f34db43c32feb35bd5f0eca878b
Evghenu Calcutin <evgheni.calcutin@gmail.com>
2022-12-14 15:35:22 +0200

    Eliminate layout shift

--- src/server/render.tsx
+++ src/server/render.tsx
@@ -2,23 +2,29 @@ import path from 'node:path';
 import fs from 'node:fs';
 
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 import App from '../App';
 
 const render = async () => {
   return new Promise((resolve, reject) => {
+    const css = new ServerStyleSheet();
     const html = path.resolve('./build/index.html');
-    const rendered = renderToString(<App />);
-
+    const rendered = renderToString(
+      <StyleSheetManager sheet={css.instance}>
+        <App />
+      </StyleSheetManager>
+    );
+    const styleTags = css.getStyleTags();
+    css.seal();
     fs.readFile(html, 'utf8', (err, html) => {
       if (err) {
         reject(err);
       }
       resolve(
-        html.replace(
-          '<div id="root"></div>',
-          `<div id="root">${rendered}</div>`
-        )
+        html
+          .replace(/<\/head>/, `${styleTags}</head>`)
+          .replace('<div id="root"></div>', `<div id="root">${rendered}</div>`)
       );
     });
   });
