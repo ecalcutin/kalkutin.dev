0a1f334edd39f3fa39912fdbf4e24b71ba7a4bbd
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-06 16:07:56 +0300

    feat(): Show version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useLayoutEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle, theme } from 'theme';
 import HomePage from 'pages/HomePage';
 import Layout from 'components/Layout';
 
+import packageJson from '../package.json';
+
 const App = () => {
+  useLayoutEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(`Version: ${packageJson.version}`);
+  }, []);
   return (
     <Fragment>
       <ThemeProvider theme={theme}>
