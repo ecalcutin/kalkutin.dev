30d2d24ddba4a3c8df39261f44664c98e3d08f25
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-17 02:29:50 +0300

    feat(): Move Logo as standalone component

---
+++ src/client/components/Header/components/logo/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+import { WebsiteLogo } from './website-logo';
+
+export const Logo: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <a href='/' aria-label='home'>
+        <WebsiteLogo />
+      </a>
+    </div>
+  );
+};
