8c21a10f425595c923daa9c243e74bee6f7a58ba
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-20 02:40:47 +0300

    feat(): print version

--- src/client/pages/HomePage/index.tsx
+++ src/client/pages/HomePage/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+
+import { version } from '../../../../package.json';
 
 import { Hero } from './components/Hero';
 import styles from './styles.module.css';
 
 export const HomePage: React.FC = () => {
+  useLayoutEffect(() => {
+    // eslint-disable-next-line
+    console.log(`Version: ${version}`);
+  }, []);
+
   return (
     <main className={styles.container}>
       <Hero />
