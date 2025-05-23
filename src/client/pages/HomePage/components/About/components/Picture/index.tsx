1cda2d40403a1e204eef5b28a127f198cd404ff7
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-23 04:21:24 +0300

    fix(): Remove redundant div

--- src/client/pages/HomePage/components/About/components/Picture/index.tsx
+++ src/client/pages/HomePage/components/About/components/Picture/index.tsx
@@ -8,10 +8,8 @@ type Props = {
 
 export const Picture: React.FC<Props> = props => {
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <img src={props.src} width={300} height={300} alt='Evgenii Kalkutin' />
-      </div>
+    <div className={styles.wrapper}>
+      <img src={props.src} width={300} height={300} alt='Evgenii Kalkutin' />
     </div>
   );
 };
