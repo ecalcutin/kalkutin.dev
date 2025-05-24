2405a0ff03465c8a94cd2310b84ba85641623a85
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-24 20:32:52 +0300

    feat(): Adjust media query for main

--- src/client/layouts/MainLayout/index.tsx
+++ src/client/layouts/MainLayout/index.tsx
@@ -2,13 +2,15 @@ import React, { PropsWithChildren } from 'react';
 
 import { Header } from 'client/components/Header';
 
+import styles from './layout.module.css';
+
 type Props = PropsWithChildren;
 
 export const MainLayout: React.FC<Props> = props => {
   return (
     <div>
       <Header />
-      <main>{props.children}</main>
+      <main className={styles.layout}>{props.children}</main>
     </div>
   );
 };
